13b7cfd74f7da8d66f4f34be238152e9f230790e
Nghia <nghiank@hotmail.com>
2021-06-12 17:14:55 -0700

    Object

--- library/utilities/objects.py
+++ library/utilities/objects.py
@@ -2,7 +2,6 @@ class Work:
     work_id = None
     job = None
     pid = None
-    plot_id = None
     
 
     temporary_drive = None
@@ -16,9 +15,6 @@ class Work:
 
     phase_times = {}
     total_run_time = None
-
-    completed = False
-
+    
     progress = 0
-    temp_file_size = 0
-    k_size = None
+    num_plots = None
