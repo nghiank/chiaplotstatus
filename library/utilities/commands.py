94a4b829fd94552b1d8de81e2e765b1fc5ac80d2
Nghia <nghiank@hotmail.com>
2021-06-17 19:47:43 -0700

    Update view

--- library/utilities/commands.py
+++ library/utilities/commands.py
@@ -9,44 +9,43 @@ from datetime import datetime
 
 
 def view_manager(loop=True):
-	running_work = get_running_plots()
-	drives = {'temp': [], 'temp2': [], 'dest': []}
-	print(running_work)
-	system_drives = get_system_drives()
-	for pid in running_work.keys():
-		work = running_work[pid]
-		directories = {
-            'dest': work.destination_drive,
-            'temp': work.temporary_drive,
-            'temp2': work.temporary2_drive,
-        }
-		for key, directory_list in directories.items():
-			if directory_list is None:
-				continue
-			if not isinstance(directory_list, list):
-				directory_list = [directory_list]
-			for directory in directory_list:
-				drive = identify_drive(file_path=directory, drives=system_drives)
-				if drive in drives[key]:
-					continue
-				drives[key].append(drive)		
-	print(drives)
-	last_notification = { 
+    last_notification = { 
 		'temperature' : datetime.now(),
 		'plot_nearly_full': datetime.now(),
 		'num_jobs': datetime.now()
 	}
-	config_info = get_config_info()	
-	while True:
-		try:
-			print_view(
+    config_info = get_config_info()	
+    while True:
+        try:
+            running_work = get_running_plots()
+            drives = {'temp': [], 'temp2': [], 'dest': []}
+            print(running_work)
+            system_drives = get_system_drives()
+            for pid in running_work.keys():
+                work = running_work[pid]
+                directories = {
+                    'dest': work.destination_drive,
+                    'temp': work.temporary_drive,
+                    'temp2': work.temporary2_drive,
+                }
+                for key, directory_list in directories.items():
+                    if directory_list is None:
+                        continue
+                    if not isinstance(directory_list, list):
+                        directory_list = [directory_list]
+                    for directory in directory_list:
+                        drive = identify_drive(file_path=directory, drives=system_drives)
+                        if drive in drives[key]:
+                            continue
+                        drives[key].append(drive)		
+            print_view(
 				running_work=running_work,
 				drives=drives, 
 				config_info=config_info, 
 				last_notification=last_notification)
-			time.sleep(60)
-		except KeyboardInterrupt:
-			print("Stopped view.")
-			exit()
+            time.sleep(60)
+        except KeyboardInterrupt:
+            print("Stopped view.")
+            exit()
 		
 
