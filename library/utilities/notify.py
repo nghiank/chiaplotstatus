8c9169e8f493785d0e8732936843bf1d0247a88b
Nghia <nghiank@hotmail.com>
2021-06-20 10:29:14 -0700

    Update check night time notification

--- library/utilities/notify.py
+++ library/utilities/notify.py
@@ -2,10 +2,11 @@ from library.utilities.telegram import send
 import platform
 from datetime import datetime, time
 
-def check_if_night_time():
-	now = datetime.now()
-	now_time = now.time()
-	return now_time >= time(23,00) and now_time <= time(8,00)
+def check_night_time():
+    now = datetime.now()
+    now_time = now.time()
+    return (now_time >= time(23,00) and now_time <= time(23,59,59)) or (now_time >= time(0,0) and now_time <= time(8,00))
+
 
 def notify_num_jobs(job_data, config_info, last_notification):
 	if check_if_night_time():		
